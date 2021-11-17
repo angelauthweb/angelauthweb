6f7e370e0f69def6bf5ac47524e086cf0952d20a
angelauthweb <94419844+angelauthweb@users.noreply.github.com>
2021-11-17 14:18:04 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyAknrvw3brgo7qxEyMk3jzsE02CyJKWo5c",
-  authDomain: "fbauten.firebaseapp.com",
-  projectId: "fbauten",
-  storageBucket: "fbauten.appspot.com",
-  messagingSenderId: "902838344264",
-  appId: "1:902838344264:web:221d59cd0ba838db98567d",
-  measurementId: "G-52E0LME3E0"
+  apiKey: "AIzaSyDiQF58_NQbAJ3e_pljgHesN4x47HbtfRU",
+  authDomain: "auth-0027.firebaseapp.com",
+  projectId: "auth-0027",
+  storageBucket: "auth-0027.appspot.com",
+  messagingSenderId: "861736195948",
+  appId: "1:861736195948:web:1b37d92ff9db16adef8549",
+  measurementId: "G-LNPNYZGM7S"
     });
